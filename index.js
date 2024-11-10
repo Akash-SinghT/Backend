387a3859458c83026b32276684b4fa77fe8539dc
Akash-SinghT <sakashh165@gmail.com>
2024-11-10 09:24:20 +0530

    added channel name

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.listen(3000);
