91f0d925d3b0048e0f9f01a617f25964a60b6fb7
Akash-SinghT <sakashh165@gmail.com>
2024-11-11 08:07:55 +0530

    added channel name

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const usermodel = require("./models/user");
+const dbconnection = require("./config/db");
 const morgan = require("morgan");
 const app = express();
 app.set("view engine", "ejs");
@@ -13,6 +15,48 @@ app.get("/", (req, res) => {
 });
 app.get("/about", (req, res) => {
   res.send("About");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  await usermodel.create({
+    username: username,
+    email: email,
+    password: password,
+  });
+
+  res.send("Registered");
+});
+app.get("/get-users", (req, res) => {
+  usermodel
+    .find({
+      username: "a",
+    })
+    .then((users) => {
+      res.send(users);
+    });
+});
+app.get("/update", async (req, res) => {
+  await usermodel.findOneAndUpdate(
+    {
+      username: "akash-singh",
+    },
+    {
+      email: "akash@gmail.com",
+    }
+  );
+});
+
+app.get("/delete", async (req, res) => {
+  await usermodel.findOneAndDelete({
+    username: "akash-singh",
+  });
+  res.send("dleeted");
 });
 app.post("/get-form-data", (req, res) => {
   console.log(req.body);
